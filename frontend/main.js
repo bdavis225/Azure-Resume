7bba75934aa046bb1a708af16ef8fac7f5540300
bdavis225 <blazeisbucc@gmail.com>
2023-10-09 17:38:41 -0700

    Created Azure function api

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,11 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response=> {
+    fetch(localFunctionApi).then(response=> {
         return response.json()
     }).then(response=>{
         console.log("Website called function API.");
